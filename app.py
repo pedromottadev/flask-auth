5bf09c39d5b9f37dc6102add2821b4da8530aca5
pedromottadev <pedromarquesmotta@outlook.com>
2025-08-21 21:59:51 -0400

    implementing authentication and crud users

---
+++ app.py
@@ -0,0 +1,99 @@
+import os
+from flask import Flask,request,jsonify
+from models.user import User
+from database import db
+from flask_login import LoginManager,login_user,current_user,logout_user,login_required
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'database.db')}"
+
+
+login_manager = LoginManager()
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+     return User.query.get(user_id)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+                login_user(user)
+                print(f"{current_user.is_authenticated}autenticado !!")
+                return jsonify({"message": "autenticação realizada com sucesso"}), 200
+           
+    return jsonify({"message": "credenciais invalidas"}), 400
+
+
+@app.route("/logout",methods=["GET"])
+@login_required
+def logout():
+     logout_user()
+     return jsonify({"message": "logout realizado com sucesso !"})
+
+
+
+@app.route('/user',methods=["POST"])
+def create_user():
+     data = request.json
+     username = data.get("username")
+     password = data.get("password")
+
+     if username and password:
+          user = User(username=username, password=password)
+          db.session.add(user)
+          db.session.commit()
+          return jsonify({"message":"usuario cadastrado com sucesso"})
+     return jsonify({"message":"credenciais invalidas"}), 401
+
+
+@app.route('/user/<int:id_user>',methods=['GET'])
+@login_required
+def read_user(id_user):
+     user = User.query.get(id_user)
+
+     if user:
+          return {"username": user.username}
+     return jsonify({"message":"usuario nao encontrado"}), 404
+
+
+@app.route('/user/<int:id_user>',methods=['PUT'])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get("password"):
+          user.password = data.get("password")
+          db.session.commit()
+          return jsonify({"message": f"usuario {user.username} atualizado com sucesso"})
+    
+    return jsonify({"message":"usuario nao encontrado"}), 404
+
+@app.route('/user/<int:id_user>',methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+    if id_user == current_user.id:
+         return jsonify({"message":"delação nao permitida"})
+    if user:
+         db.session.delete(user)
+         db.session.commit()
+         return jsonify({"message": f"usuario {user.username} deletado com sucesso"})
+    
+    return jsonify({"message":"usuario nao encontrado"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
